7807cfdab834dd93dc727d173ad33554c6ae2931
Yolanda Wiersma <38360891+nlboreal@users.noreply.github.com>
2020-02-20 14:43:07 -0330

    Add files via upload

---
+++ 01-QuantModuleIntro.R
@@ -0,0 +1,21 @@
+---
+title: "QuantModuleIntro"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Quantitative Methods Overview
+
+Biology, including ecology and conservation, is becoming increasingly quantitative. Familiarity with a range of statistical tools (frequentist, multivariate, Bayesian) is expected. Many researchers draw on quantitative methods; whether it is process or mathematical modelling, programming, bioinformatics, or bioeconomic analyses. We will only touch on key concepts in quantitative methods; in-depth training would require multiple full-semester courses. Here, we will carry out a few simple exercise that mimics some of the quantitative skills that you need to be a successful ecologist. These are:
+
+1.	Use R!
+2.	Manage data sets
+3.	Plot data
+4.	Fit a model to data
+
+In this module there will be a small assignment with each of the above objectives. These are detailed below along with the percentage of your final grade that this is worth (the entire module is worth 10% of your final mark).
+
+
